18822462e3da1ae7a02cb4552be1553be469fa37
Hodgdonc <chloe.hodgdon@maine.edu>
2024-01-28 00:39:17 -0500

    Added ability for teams to read/write to JSON
    Updated general widgets and added the ability to read and write team JSON values to the related Entry widgets. NOTE: this is not a permanent solution, as it currently doesn't account for creating teams, just updating existing teams.

--- ui_advanced_config.py
+++ ui_advanced_config.py
@@ -5,6 +5,7 @@ import queue
 import cProfile
 import logging
 import loader
+import json
 
 from ui_widgets import *
 
@@ -38,11 +39,11 @@ class UISettings(tk.Toplevel):
         self.titleLabel = uiLabel(master=self.bottomFrame, text="Advanced Settings")
         
         self.teamsLabel = uiLabel(master=self.teamsColumn, text="Teams")
-        self.teamsUpdateButton = uiButton(master=self.teamsColumn,command="", text="update")
         self.teamSizeLabel = uiLabel(master=self.teamsColumn, text="Size:")
         self.teamSizeEntry = uiEntry(master=self.teamsColumn)
         self.teamNameLabel = uiLabel(master=self.teamsColumn,text="Name:")
         self.teamNameEntry = uiEntry(master=self.teamsColumn)
+        
 
         self.componentsLabel = uiLabel(master=self.componentsColumn, text="Components")
         self.componentsUpdateButton = uiButton(master=self.componentsColumn,command="",text="Update")
@@ -86,31 +87,47 @@ class UISettings(tk.Toplevel):
         self.teamSizeEntry.grid(row=3,column=2,sticky="nsew")
         self.teamNameLabel.grid(row=4,column=1,sticky="nsew")
         self.teamNameEntry.grid(row=4,column=2,sticky="nsew")
-        self.teamsUpdateButton.grid(row=8,column=1,columnspan=2,sticky="nsew")
 
-        self.squadFrame = uiQuietFrame(master=self.teamsColumn, borderwidth=5,  relief="ridge",sticky="nsew")
-        self.squadFrame.grid(row=5,column=1,columnspan=2,rowspan=2,sticky="nsew", ipadx=0,ipady=0)
-        self.callsignLabel = uiLabel(master=self.squadFrame,text="Callsign:")
+        self.agentFrame = uiQuietFrame(master=self.teamsColumn, borderwidth=5,  relief="ridge",sticky="nsew")
+        self.agentFrame.grid(row=5,column=1,columnspan=2,rowspan=2,sticky="nsew", ipadx=0,ipady=0)
+        self.callsignLabel = uiLabel(master=self.agentFrame,text="Callsign:")
         self.callsignLabel.grid(row=1,column=1,sticky="nsew")
-        self.callsignEntry = uiEntry(master=self.squadFrame)
+        self.callsignEntry = uiEntry(master=self.agentFrame)
         self.callsignEntry.grid(row=1,column=2,sticky="nsew")
-        self.squadLabel = uiLabel(master=self.squadFrame,text="Squad:")
+        self.squadLabel = uiLabel(master=self.agentFrame,text="Squad:")
         self.squadLabel.grid(row=2,column=1,sticky="nsew")
-        self.squadEntry = uiEntry(master=self.squadFrame)
+        self.squadEntry = uiEntry(master=self.agentFrame)
         self.squadEntry.grid(row=2,column=2,sticky="nsew")
-        self.squadObjectLabel = uiLabel(master=self.squadFrame,text="Object:")
-        self.squadObjectLabel.grid(row=3,column=1,sticky="nsew")
-        self.squadObjectEntry = uiEntry(master=self.squadFrame)
-        self.squadObjectEntry.grid(row=3,column=2,sticky="nsew")
+        self.agentObjectLabel = uiLabel(master=self.agentFrame,text="Object:")
+        self.agentObjectLabel.grid(row=3,column=1,sticky="nsew")
+        self.agentObjectEntry = uiEntry(master=self.agentFrame)
+        self.agentObjectEntry.grid(row=3,column=2,sticky="nsew")
+        self.aiFileLabel = uiLabel(master=self.agentFrame,text="AI File:")
+        self.aiFileLabel.grid(row=4,column=1)
+        self.aiFileEntry = uiEntry(master=self.agentFrame)
+        self.aiFileEntry.grid(row=4,column=2)
 
-
+        self.teamData=self.ldr.team_templates
+        print(self.teamData)
+        self.teamNames = self.ldr.getTeamNames()
+        self.teamNameEntry.insert(0,self.teamNames[0])
+        self.teamSizeEntry.insert(0,self.teamData[self.teamNames[0]]['size'])
+        self.callsignEntry.insert(0,self.teamData[self.teamNames[0]]['agent_defs'][0]['callsign'])
+        self.squadEntry.insert(0,self.teamData[self.teamNames[0]]['agent_defs'][0]['squad'])
+        self.agentObjectEntry.insert(0,self.teamData[self.teamNames[0]]['agent_defs'][0]['object'])
+        self.aiFileEntry.insert(0,self.teamData[self.teamNames[0]]['agent_defs'][0]['AI_file'])
         
+        self.teamsUpdateButton = uiButton(master=self.teamsColumn,command=self.updateTeamsJSON, text="update")
+        self.teamsUpdateButton.grid(row=7,column=1,columnspan=2,sticky="nsew")
+
+
         self.componentsColumn.pack(side=tk.LEFT,fill=tk.BOTH,expand=True,padx=10,pady=10)
         self.componentsLabel.grid(row=2,column=1,columnspan=2,sticky="nsew")
         self.componentsIDLabel.grid(row=3,column=1,sticky="nsew")
         self.componentsIDEntry.grid(row=3,column=2,sticky="nsew")
         self.componentsNameLabel.grid(row=4,column=1,sticky="nsew")
         self.componentsNameEntry.grid(row=4,column=2,sticky="nsew")
+        self.componentsUpdateButton.grid(row=7,column=1,columnspan=2)
 
         self.objectsColumn.pack(side=tk.LEFT,fill=tk.BOTH,expand=True,padx=10,pady=10)
         self.objectsLabel.grid(row=2,column=1,columnspan=2,sticky="nsew")
@@ -124,6 +141,7 @@ class UISettings(tk.Toplevel):
         self.objectsFillDeadEntry.grid(row=6,column=2,sticky="nsew")
         self.objectsTextLabel.grid(row=7,column=1,sticky="nsew")
         self.objectsTextEntry.grid(row=7,column=2,sticky="nsew")
+        self.objectsUpdateButton.grid(row=10,column=1,columnspan=2)
 
         self.mapsColumn.pack(side=tk.LEFT,fill=tk.BOTH,expand=True,padx=10,pady=10)
         self.mapsLabel.grid(row=1,column=1,columnspan=2,sticky="nsew")
@@ -135,13 +153,22 @@ class UISettings(tk.Toplevel):
         self.mapsEdgeObjIDEntry.grid(row=4,column=2,sticky="nsew")
         self.mapsDescLabel.grid(row=5,column=1,sticky="nsew")
         self.mapsDescEntry.grid(row=5,column=2,sticky="nsew")
-
-        teamName = self.ldr.getTeamNames()[0]
-        self.mapsNameEntry.insert(0,teamName)
+        self.mapsUpdateButton.grid(row=9,column=1,columnspan=2)
 
 
-    def updateTeamsJSON():
-        return
+    def updateTeamsJSON(self): 
+        self.teamData[self.teamNameEntry.get()]['size'] = self.teamSizeEntry.get()
+        self.teamData[self.teamNameEntry.get()]['agent_defs'][0]['callsign'] = self.callsignEntry.get()
+        self.teamData[self.teamNameEntry.get()]['name'] = self.teamNameEntry.get()
+
+        print(self.teamNameEntry.get())
+        self.teamsJSON = json.dumps(self.teamData, indent=4)
+        print(self.teamsJSON)
+
+        with open('settings/teams.json', 'w') as f:
+            f.write(self.teamsJSON)
+        f.close()
+
     
     def updateComponentsJSON():
         return
