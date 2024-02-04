6a2ddb4d1f26990283b5dba06d14deadff6e935b
HenryKindler <123681101+HenryKindler@users.noreply.github.com>
2024-02-03 22:08:48 -0500

    Combined buttons in Set-up Page
    This update combined the buildUI and RunSim buttons, as well as selecting a map happening as soon as user clicks map.

--- ui_setup.py
+++ ui_setup.py
@@ -16,15 +16,22 @@ from zexceptions import *
 from ui_widgets import *
 
 
+#Henry Taking some stuff from stack overflow:
+def callback(event):
+    selection = event.widget.curselection()
+    if selection:
+
+        print("Hello world") #okay so this works, but self.selectMap wont sork cuase on self
+
 class UISetup(tk.Frame):
     def __init__(self,master=None,logger=None):
         super().__init__(master)
         self.master = master
         self.logger = logger
 
-        self.configure(bg=DARKCOLOR)
-        self.pack()
-        self.master.title("MAIA - Maine AI Arena")
+        self.configure(bg=DARKCOLOR) #bg is background i think
+        self.pack() 
+        self.master.title("MAIA - Maine AI Arena") #top of window tool bar
 
         # Create the msgr objs
         self.msg_queue = queue.Queue()
@@ -50,111 +57,8 @@ class UISetup(tk.Frame):
     def Run(self):
         self.mainloop()
 
-    def BuildUI(self):
-        #######################################################################
-        ## MAP UI
-        #######################################################################
-        self.mapSelectFrame = uiQuietFrame(master=self)
-        self.mapSelectFrame.pack(fill=tk.BOTH,expand=True,padx=10,pady=10)
-
-        self.lbMaps = uiListBox(self.mapSelectFrame)
-        self.lbMaps.pack(side=tk.LEFT,fill=tk.BOTH,expand=True)
-
-        self.btnSelectMap = uiButton(master=self.mapSelectFrame,command=self.selectMap,text="Select Map")
-        self.btnSelectMap.pack(side=tk.LEFT,fill=tk.BOTH,expand=False)
-
-        self.txtMapInfo = uiScrollText(self.mapSelectFrame)
-        self.txtMapInfo.pack(side=tk.LEFT,fill=tk.BOTH,expand=True)
-        self.txtMapInfo.insert(tk.END,"No map info")
-
-        self.updateMapNames()
-        #######################################################################
-        ## TEAM UI
-        ######################################################################
-        self.teamFrame = uiQuietFrame(master=self)
-        self.teamFrame.pack(fill=tk.BOTH,expand=True,padx=10,pady=10)
-
-        self.teamPoolFrame = uiQuietFrame(master=self.teamFrame)
-        self.teamPoolFrame.pack(side=tk.LEFT,fill=tk.BOTH,expand=True)
-
-        self.teamPoolLeftFrame = uiQuietFrame(master=self.teamPoolFrame)
-        self.teamPoolLeftFrame.pack(side=tk.LEFT,fill=tk.BOTH,expand=True)
-
-        self.lblTeamPool = uiLabel(master=self.teamPoolLeftFrame,text="TEAM POOL")
-        self.lblTeamPool.pack(fill=tk.BOTH,side=tk.TOP,expand=True)
-
-        self.lbTeams = uiListBox(self.teamPoolLeftFrame)
-        self.lbTeams.pack(side=tk.TOP,fill=tk.BOTH,expand=True)
-
-        self.teamPoolRightFrame = uiQuietFrame(master=self.teamPoolFrame)
-        self.teamPoolRightFrame.pack(side=tk.RIGHT,fill=tk.BOTH,expand=True)
-
-        self.lblSides= uiLabel(master=self.teamPoolRightFrame,text="SIDES")
-        self.lblSides.pack(fill=tk.BOTH,side=tk.TOP,expand=True)
-
-        self.lbSideNames = uiListBox(self.teamPoolRightFrame)
-        self.lbSideNames.pack(side=tk.BOTTOM,fill=tk.BOTH,expand=True)
-
-
-        # self.lbTeams = tk.Listbox(self.teamPoolFrame,selectmode=tk.SINGLE,exportselection=0,relief=tk.FLAT,bg=self.main_bgcolor,fg=self.main_fgcolor,highlightthickness=0,highlightbackground="black")
-        # self.lbTeams.pack(side=tk.LEFT,fill=tk.BOTH,expand=True)
-
-        
-
-        self.playPoolFrame = uiQuietFrame(master=self.teamFrame)
-        self.playPoolFrame.pack(side=tk.RIGHT,fill=tk.BOTH,expand=True)
-
-        self.lblPlayPool = uiLabel(master=self.playPoolFrame,text="ASSIGNED TEAMS")
-        self.lblPlayPool.pack(fill=tk.BOTH,side=tk.TOP,expand=True)
-
-        self.subPlayPoolFrame = uiQuietFrame(master=self.playPoolFrame)
-        self.subPlayPoolFrame.pack(fill=tk.BOTH,side=tk.BOTTOM,expand=True)
-
-        
-
-        self.lbTeamAssignments = uiListBox(self.subPlayPoolFrame)
-        self.lbTeamAssignments.pack(side=tk.RIGHT,fill=tk.BOTH,expand=True)
-
-        self.updateTeamNames()
-
-        self.pack(fill=tk.BOTH,expand=True)
-
-        self.btnAddTeam = uiButton(master=self.teamFrame, text="Add Team >>>",command=self.addTeam)
-        self.btnAddTeam.pack(side=tk.TOP,fill=tk.BOTH,expand=True)
-
-        self.btnRemoveTeam = uiButton(master=self.teamFrame,text="<<< Remove Team", command=self.removeTeam)
-        self.btnRemoveTeam.pack(side=tk.BOTTOM,fill=tk.BOTH,expand=True)
-        #######################################################################
-        ## SIM UI
-        #######################################################################
-        self.fmSimUI = uiQuietFrame(master=self)
-        self.fmSimUI.pack(side=tk.RIGHT,fill=tk.BOTH,expand=True,padx=10,pady=10)
-
-        self.btnBuildSim = uiButton(master=self.fmSimUI,command=self.buildSim,text="Build Sim")
-        self.btnBuildSim.pack(side=tk.LEFT,fill=tk.BOTH,expand=True)
-
-        self.btnRunSimWithUI = uiButton(master=self.fmSimUI,command=self.runSimWithUI,text="Run Sim With UI")
-        self.btnRunSimWithUI.pack(side=tk.RIGHT,fill=tk.BOTH,expand=True)
-
-        # self.btnRunSimWithoutUI = tk.Button(self.fmSimUI,text="Run Sim Without UI",command=self.runSimWithoutUI)
-        # self.btnRunSimWithoutUI.pack(side=tk.RIGHT,fill=tk.BOTH,expand=True)
-
-    def updateTeamNames(self):
-        self.lbTeams.delete(0,tk.END)
-        self.lbSideNames.delete(0,tk.END)
-        self.lbTeamAssignments.delete(0,tk.END)
-        for name in self.ldr.getTeamIDs():
-            self.lbTeams.insert(tk.END,name)
-        for k,v in self.sim.getSides().items():
-            self.lbSideNames.insert(tk.END,k)
-            self.lbTeamAssignments.insert(tk.END,str(self.sim.getTeamName(k)))
-
-    def updateMapNames(self):
-        self.lbMaps.delete(0,tk.END)
-        for name in self.ldr.getMapIDs():
-            self.lbMaps.insert(tk.END,name)
-
-    def selectMap(self):
+    
+    def selectMap(self, event): # this function is what happens when you hit button "select map" #moved it up here so that it could be bound to selecting map in BuildUI
         curselection = self.lbMaps.curselection()
         if len(curselection) > 0:
             # Reset the sim
@@ -191,6 +95,146 @@ class UISetup(tk.Frame):
             
             self.updateTeamNames()
 
+    
+
+    def BuildUI(self):
+
+
+        #######################################################################
+        ## SIM UI
+        #######################################################################
+        self.fmSimUI = uiQuietFrame(master=self)
+        self.fmSimUI.pack(side=tk.BOTTOM,fill=tk.BOTH,expand=True,padx=10,pady=10) #maybe remove padding? maybe remove whole frame, and make button part of master frame?
+        
+
+        #below four lines were replaced by below fifth and sixth line
+
+        # self.btnBuildSim = uiButton(master=self.fmSimUI,command=self.buildSim,text="Build Sim")
+        # self.btnBuildSim.pack(side=tk.LEFT,fill=tk.BOTH,expand=True)
+
+        # self.btnRunSimWithUI = uiButton(master=self.fmSimUI,command=self.runSimWithUI,text="Run Sim With UI")
+        # self.btnRunSimWithUI.pack(side=tk.RIGHT,fill=tk.BOTH,expand=True)
+
+        self.startBtnFrame = uiQuietFrame(master=self.fmSimUI)
+        self.startBtnFrame.pack(side=tk.RIGHT)
+
+        self.advancedConfigBtnFrame = uiQuietFrame(master=self.fmSimUI)
+        self.advancedConfigBtnFrame.pack(side=tk.LEFT)
+
+        self.btnBuildAndRunSim = uiButton(master=self.startBtnFrame,command=self.buildAndRunSim,text="Start Game") #Start Game Button
+        self.btnBuildAndRunSim.pack(side=tk.RIGHT,padx=50,fill=tk.BOTH)
+
+        self.btnAdvancedConfig = uiButton(master=self.advancedConfigBtnFrame,command=self.buildAndRunSim,text="Advanced Config") #have to change command later to navigate to advanced config
+        self.btnAdvancedConfig.pack(side=tk.LEFT,padx=50,fill=tk.BOTH)
+
+        # self.btnRunSimWithoutUI = tk.Button(self.fmSimUI,text="Run Sim Without UI",command=self.runSimWithoutUI)
+        # self.btnRunSimWithoutUI.pack(side=tk.RIGHT,fill=tk.BOTH,expand=True)
+
+
+        #######################################################################
+        ## MAP UI
+        #######################################################################
+        self.mapSelectFrame = uiQuietFrame(master=self)  #map section 
+        self.mapSelectFrame.pack(side=tk.LEFT,fill=tk.BOTH,expand=True,padx=10,pady=10) #HK set side to left 
+
+        self.lbMaps = uiListBox(self.mapSelectFrame)  #this is the box that says Map 1 lbMaps
+        self.lbMaps.pack(side=tk.TOP,fill=tk.BOTH,expand=True)
+
+        self.lbMaps.bind("<<ListboxSelect>>", self.selectMap) #this calls selectMap(self, whateverMapIsSelectedInTheBox)
+
+        #self.btnSelectMap = uiButton(master=self.mapSelectFrame,command=self.selectMap,text="Select Map") #button 
+        #self.btnSelectMap.pack(side=tk.BOTTOM,fill=tk.BOTH,expand=False)
+
+        self.txtMapInfo = uiScrollText(self.mapSelectFrame) #info on selcted frame
+        self.txtMapInfo.pack(side=tk.BOTTOM,fill=tk.BOTH,expand=True)
+        self.txtMapInfo.insert(tk.END,"No map info")
+
+        self.updateMapNames()# this function call gets Map 1 (later other maps) into the box
+        
+        #self.selectMap()
+
+        #######################################################################
+        ## TEAM UI
+        ######################################################################
+        self.teamFrame = uiQuietFrame(master=self) #team frame is everything team UI #top line creates frame
+        self.teamFrame.pack_propagate(0)
+        self.teamFrame.pack(side=tk.RIGHT,fill=tk.BOTH,expand=True,padx=10,pady=10) #bottom line is frame settings
+
+        self.teamPoolFrame = uiQuietFrame(master=self.teamFrame) #Left half of team frame
+        self.teamPoolFrame.pack(side=tk.TOP,fill=tk.BOTH,expand=True)
+
+        self.teamPoolLeftFrame = uiQuietFrame(master=self.teamPoolFrame) # part of pool frame, pool section
+        self.teamPoolLeftFrame.pack(side=tk.LEFT,fill=tk.BOTH,expand=True)
+
+        self.lblTeamPool = uiLabel(master=self.teamPoolLeftFrame,text="TEAM POOL") #label that says "Team Pool" section of pool left frame
+        self.lblTeamPool.pack(fill=tk.BOTH,side=tk.TOP,expand=True)
+
+        self.lbTeams = uiListBox(self.teamPoolLeftFrame)
+        self.lbTeams.pack(side=tk.BOTTOM,fill=tk.BOTH,expand=True)
+
+        #self.lbTeams.bind("<<ListboxSelect>>", self.)
+
+        self.teamPoolRightFrame = uiQuietFrame(master=self.teamPoolFrame) #pool right frame is the sides section, also part of Pool Frame
+        self.teamPoolRightFrame.pack(side=tk.BOTTOM,fill=tk.BOTH,expand=True)
+
+        self.lblSides= uiLabel(master=self.teamPoolRightFrame,text="SIDES")
+        self.lblSides.pack(fill=tk.BOTH,side=tk.TOP,expand=True)
+
+        self.lbSideNames = uiListBox(self.teamPoolRightFrame)
+        self.lbSideNames.pack(side=tk.BOTTOM,fill=tk.BOTH,expand=True)
+
+
+        # self.lbTeams = tk.Listbox(self.teamPoolFrame,selectmode=tk.SINGLE,exportselection=0,relief=tk.FLAT,bg=self.main_bgcolor,fg=self.main_fgcolor,highlightthickness=0,highlightbackground="black")
+        # self.lbTeams.pack(side=tk.LEFT,fill=tk.BOTH,expand=True)
+
+        
+
+        self.playPoolFrame = uiQuietFrame(master=self.teamFrame) # Assigned is other half of team frame, "Play pool" is teams that are playing
+        self.playPoolFrame.pack(side=tk.BOTTOM,fill=tk.BOTH,expand=True)
+
+        self.lblPlayPool = uiLabel(master=self.playPoolFrame,text="ASSIGNED TEAMS")
+        self.lblPlayPool.pack(fill=tk.BOTH,side=tk.TOP,expand=True)
+
+        self.subPlayPoolFrame = uiQuietFrame(master=self.playPoolFrame)
+        self.subPlayPoolFrame.pack(fill=tk.BOTH,side=tk.BOTTOM,expand=True)
+
+        
+
+        self.lbTeamAssignments = uiListBox(self.subPlayPoolFrame)
+        self.lbTeamAssignments.pack(side=tk.RIGHT,fill=tk.BOTH,expand=True)
+
+        self.updateTeamNames()
+
+        self.pack(fill=tk.BOTH,expand=True)
+
+        self.btnAddTeam = uiButton(master=self.teamFrame, text="Add Team >>>",command=self.addTeam)
+        self.btnAddTeam.pack(side=tk.TOP,fill=tk.BOTH,expand=True)
+
+        self.btnRemoveTeam = uiButton(master=self.teamFrame,text="<<< Remove Team", command=self.removeTeam)
+        self.btnRemoveTeam.pack(side=tk.BOTTOM,fill=tk.BOTH,expand=True)
+
+        
+        
+        
+        
+
+    def updateTeamNames(self):
+        self.lbTeams.delete(0,tk.END)
+        self.lbSideNames.delete(0,tk.END)
+        self.lbTeamAssignments.delete(0,tk.END)
+        for name in self.ldr.getTeamIDs():
+            self.lbTeams.insert(tk.END,name)
+        for k,v in self.sim.getSides().items():
+            self.lbSideNames.insert(tk.END,k)
+            self.lbTeamAssignments.insert(tk.END,str(self.sim.getTeamName(k)))
+
+    def updateMapNames(self): #this function gets all maps from the JSON and puts them in the box list
+        self.lbMaps.delete(0,tk.END)
+        for name in self.ldr.getMapIDs():
+            self.lbMaps.insert(tk.END,name)
+
+
+   
     def addTeam(self):
         if self.sim.hasMap():
             team_index = self.lbTeams.curselection()
@@ -207,6 +251,7 @@ class UISetup(tk.Frame):
                 self.logger.error("App::addTeam() - No side or team selected.")
         else:
             self.logger.error("App::addTeam() - Sim is missing the map.")
+
     def removeTeam(self):
         # side_index = self.lbSideNames.curselection()
         # if len(side_index) > 0:
@@ -222,7 +267,6 @@ class UISetup(tk.Frame):
             self.sim.delTeamName(side_ID)
             self.updateTeamNames()
 
-
     def buildSim(self):
         try:
             self.sim.buildSim(self.ldr)
@@ -231,10 +275,24 @@ class UISetup(tk.Frame):
         else:
             tk.messagebox.showinfo(title="Success",message="Sim build was successful.")
 
-    def runSimWithUI(self):
-        map_width=self.sim.getMap().getData('width')
-        map_height=self.sim.getMap().getData('height')
-        self.UIMap = ui_sim.UISim(map_width,map_height,self.sim,self.omsgr,self,self.logger)
+    #def runSimWithUI(self):
+     #   map_width=self.sim.getMap().getData('width')
+      #  map_height=self.sim.getMap().getData('height')
+       # self.UIMap = ui_sim.UISim(map_width,map_height,self.sim,self.omsgr,self,self.logger)
 
-    def runSimWithoutUI(self):
-        self.sim.runSim(self.ldr.getMainConfigData('no_ui_max_turns'))
+
+    #def runSimWithoutUI(self):
+     #   self.sim.runSim(self.ldr.getMainConfigData('no_ui_max_turns'))
+
+    def buildAndRunSim(self): #combined above two methods
+        try:
+            self.sim.buildSim(self.ldr)
+        except BuildException as e:
+            tk.messagebox.showinfo(title="Build Exception",message=e)
+        else:
+            #tk.messagebox.showinfo(title="Success",message="Sim build was successful.")    --this line makes a pop up
+            map_width=self.sim.getMap().getData('width')
+            map_height=self.sim.getMap().getData('height')
+            self.UIMap = ui_sim.UISim(map_width,map_height,self.sim,self.omsgr,self,self.logger)
+
+    
