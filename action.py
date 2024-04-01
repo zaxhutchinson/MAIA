16ab3fc4e24979b393719c9d9fe952ba36739ef5
Sophie Kummer <sophie.kummer@maine.edu>
2024-04-01 12:54:05 -0400

    Refactoring action.py to meet code conventions

--- action.py
+++ action.py
@@ -1,21 +1,33 @@
 class Action:
     def __init__(self):
+        """Sets action type and data to default"""
         self.type = None
         self.data = {}
 
-    def setType(self, typ):
+    def set_type(self, typ):
+        """Setter of type"""
         self.type = typ
 
-    def getType(self):
+    def get_type(self):
+        """Getter of type"""
         return self.type
 
-    def setData(self, data):
+    def set_data(self, data):
+        """Setter of data
+
+        Replaced currently stored data
+        """
         self.data = data
 
-    def addData(self, key, value):
+    def add_data(self, key, value):
+        """Add key and value of data
+
+        Adds to currently stored data
+        """
         self.data[key] = value
 
-    def getData(self, key):
+    def get_data(self, key):
+        """Getter of data"""
         if key in self.data:
             return self.data[key]
         else:
