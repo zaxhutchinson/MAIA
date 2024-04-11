c21293e1c36bf903293e15962c6d8cdb8b3b5a0d
Hodgdonc <chloe.hodgdon@maine.edu>
2024-04-11 19:14:28 -0400

    Linting fix

--- teams/TeamZach/Zach_AI.py
+++ teams/TeamZach/Zach_AI.py
@@ -1,6 +1,7 @@
 import ai_helpers as aih
 import random
 
+
 class AI:
     def __init__(self):
         None
@@ -22,14 +23,12 @@ class AI:
         self.slots_by_ctype = None
         self.tick = 0
 
-
     def initRunTime(self, view):
         self.is_initd = True
         self.slots_by_ctype = aih.getSlotIDsByCtype(view)
 
     def getSlot(self, ctype):
         return self.slots_by_ctype[ctype][0]
-
 
     def updateMap(self, view):
 
@@ -41,33 +40,25 @@ class AI:
             pass
 
     def distance(self, locA, locB):
-        return ((locB[0]-locA[0])**2 + (locB[1]-locA[1])**2)**0.5
-
+        return ((locB[0] - locA[0]) ** 2 + (locB[1] - locA[1]) ** 2) ** 0.5
 
     def checkForEnemyObj(self, view, objname):
-        pings = aih.searchRadarForObjname(view, 'Blue Tank')
+        pings = aih.searchRadarForObjname(view, "Blue Tank")
 
         # If we see the blue tank, shoot!
         if len(pings) > 0:
-            if aih.canWeaponFire(view, self.getSlot('FixedGun')):
-                self.cmd_maker.addCmd(
-                    0, self.getSlot('FixedGun'),
-                    aih.CMD_Fire()
-                )
-            
-        # Always check if we need to reload.
-        if aih.doesWeaponNeedReloading(view, self.getSlot('FixedGun')):
-            self.cmd_maker.addCmd(
-                0, self.getSlot('FixedGun'),
-                aih.CMD_Reload()
-            )
+            if aih.canWeaponFire(view, self.getSlot("FixedGun")):
+                self.cmd_maker.addCmd(0, self.getSlot("FixedGun"), aih.CMD_Fire())
 
+        # Always check if we need to reload.
+        if aih.doesWeaponNeedReloading(view, self.getSlot("FixedGun")):
+            self.cmd_maker.addCmd(0, self.getSlot("FixedGun"), aih.CMD_Reload())
 
     def determineNextMove(self, view):
         pings = aih.searchRadarForObjname(view, "Indestructible Block")
         myloc = (aih.getX(view), aih.getY(view))
         if len(pings) > 0:
-            
+
             objloc = (pings[0]["x"], pings[0]["y"])
             dist = self.distance(objloc, myloc)
             engine_slots = self.slots_by_ctype["Engine"]
@@ -84,20 +75,19 @@ class AI:
                 # First, if we're turning, stop.
                 if turn_rate < 0 or turn_rate > 0:
                     self.cmd_maker.addCmd(
-                        self.tick, self.getSlot('Engine'),
-                        aih.CMD_Turn(0.0)
+                        self.tick, self.getSlot("Engine"), aih.CMD_Turn(0.0)
                     )
                 # If we're standing still, move.
                 elif speed == 0:
                     self.cmd_maker.addCmd(
-                        self.tick, self.getSlot('Engine'),
-                        aih.CMD_SetSpeed(1.0)
+                        self.tick, self.getSlot("Engine"), aih.CMD_SetSpeed(1.0)
                     )
                 # Otherwise, 10% of the time, turn a random amount left or right.
                 elif random.random() < 0.1:
                     self.cmd_maker.addCmd(
-                        self.tick, self.getSlot('Engine'),
-                        aih.CMD_Turn(random.uniform(-36.0,36.0))
+                        self.tick,
+                        self.getSlot("Engine"),
+                        aih.CMD_Turn(random.uniform(-36.0, 36.0)),
                     )
             # If we're close to a block, we want to stop moving forward
             # and turn.
@@ -105,16 +95,15 @@ class AI:
                 # Stop moving
                 if speed > 0:
                     self.cmd_maker.addCmd(
-                        self.tick, self.getSlot('Engine'),
-                        aih.CMD_SetSpeed(0.0)
+                        self.tick, self.getSlot("Engine"), aih.CMD_SetSpeed(0.0)
                     )
                 # Turn in a random direction 18 deg per turn (or 1/20th of a circle).
                 if turn_rate == 0:
                     self.cmd_maker.addCmd(
-                        self.tick, self.getSlot('Engine'),
-                        aih.CMD_Turn(random.choice([-18.0,18.0]))
+                        self.tick,
+                        self.getSlot("Engine"),
+                        aih.CMD_Turn(random.choice([-18.0, 18.0])),
                     )
-            
 
     # Implement AI here.
     # IMPORTANT: Must return commands a dictionary that follows the command
@@ -128,14 +117,13 @@ class AI:
             self.initRunTime(view)
 
             self.cmd_maker.addCmd(
-                self.tick, self.getSlot('Radar'),
-                aih.CMD_ActivateRadar()
+                self.tick, self.getSlot("Radar"), aih.CMD_ActivateRadar()
             )
 
         self.updateMap(view)
 
         self.checkForEnemyObj(view, "Blue Tank")
-            
+
         self.determineNextMove(view)
 
         return self.cmd_maker.getCmds()
