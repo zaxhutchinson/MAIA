67dd57d693fdfd7b238e2b4d6a42d693ac402298
Sophie Kummer <sophie.kummer@maine.edu>
2024-04-11 23:15:41 -0400

    WIP: ai script stuffffff

--- teams/TeamLADS/LADS_AI.py
+++ teams/TeamLADS/LADS_AI.py
@@ -23,7 +23,9 @@ class AI:
         self.slots_by_ctype = None
         self.tick = 0
         self.start_performed = False
+        self.cmd_start_iteration = 0
         self.cmd_iteration = 0
+        self.start_cmd_set = []
 
         self.start_cmds_a = [
             aih.CMD_Turn(-45),
@@ -31,7 +33,9 @@ class AI:
             aih.CMD_SetSpeed(1),
             aih.CMD_SetSpeed(1),
             aih.CMD_SetSpeed(1),
+            aih.CMD_SetSpeed(1),
             aih.CMD_Turn(45),
+            aih.CMD_Turn(0),
             aih.CMD_Turn(0),
         ]
 
@@ -42,8 +46,41 @@ class AI:
             aih.CMD_SetSpeed(1),
             aih.CMD_SetSpeed(1),
             aih.CMD_SetSpeed(1),
+            aih.CMD_SetSpeed(1),
             aih.CMD_Turn(45),
-            aih.CMD_Turn(0)
+            aih.CMD_Turn(0),
+            aih.CMD_Turn(0),
+        ]
+
+        self.cmd = [
+            aih.CMD_SetSpeed(1),
+            aih.CMD_SetSpeed(1),
+            aih.CMD_SetSpeed(1),
+            aih.CMD_SetSpeed(1),
+            aih.CMD_SetSpeed(0),
+            aih.CMD_Turn(30),
+            aih.CMD_Turn(30),
+            aih.CMD_Turn(30),
+            aih.CMD_Turn(30),
+            aih.CMD_Turn(30),
+            aih.CMD_Turn(30),
+            aih.CMD_Turn(30),
+            aih.CMD_Turn(30),
+            aih.CMD_Turn(30),
+            aih.CMD_Turn(30),
+            aih.CMD_Turn(30),
+            aih.CMD_Turn(30),
+            aih.CMD_Turn(30),
+            aih.CMD_Turn(30),
+            aih.CMD_Turn(30),
+            aih.CMD_Turn(30),
+            aih.CMD_Turn(30),
+            aih.CMD_Turn(30),
+            aih.CMD_Turn(30),
+            aih.CMD_Turn(30),
+            aih.CMD_Turn(30),
+            aih.CMD_Turn(0),
+            aih.CMD_Turn(0),
         ]
 
     def initRunTime(self, view):
@@ -70,67 +107,34 @@ class AI:
 
         # If we see the blue tank, shoot!
         if len(pings) > 0:
+            print("BANGGGGG!!!!!!!")
             if aih.canWeaponFire(view, self.getSlot("FixedGun")):
                 self.cmd_maker.addCmd(0, self.getSlot("FixedGun"), aih.CMD_Fire())
+        else:
+            print(":(")
 
         # Always check if we need to reload.
         if aih.doesWeaponNeedReloading(view, self.getSlot("FixedGun")):
             self.cmd_maker.addCmd(0, self.getSlot("FixedGun"), aih.CMD_Reload())
 
     def determineNextMove(self, view):
-        pings = aih.searchRadarForObjname(view, "Indestructible Block")
-        myloc = (aih.getX(view), aih.getY(view))
-        if len(pings) > 0:
+        self.cmd_maker.addCmd(
+            self.tick, self.getSlot("Engine"), self.cmd[self.cmd_iteration]
+        )
+        self.cmd_iteration += 1
+        if len(self.cmd) <= self.cmd_iteration:
+            self.cmd_iteration = 0
 
-            objloc = (pings[0]["x"], pings[0]["y"])
-            dist = self.distance(objloc, myloc)
-            engine_slots = self.slots_by_ctype["Engine"]
-            turn_rate = 0.0
-            speed = 0.0
-
-            for es in engine_slots:
-                engine_comp = aih.getCompBySlotID(view, es)
-                turn_rate += engine_comp["cur_turnrate"]
-                speed += engine_comp["cur_speed"]
-
-            # If we're far from an indestructible block...
-            if dist > 2:
-                # First, if we're turning, stop.
-                if turn_rate < 0 or turn_rate > 0:
-                    self.cmd_maker.addCmd(
-                        self.tick, self.getSlot("Engine"), aih.CMD_Turn(0.0)
-                    )
-                # If we're standing still, move.
-                elif speed == 0:
-                    self.cmd_maker.addCmd(
-                        self.tick, self.getSlot("Engine"), aih.CMD_SetSpeed(1.0)
-                    )
-                # Otherwise, 10% of the time, turn a random amount left or right.
-                elif random.random() < 0.1:
-                    self.cmd_maker.addCmd(
-                        self.tick,
-                        self.getSlot("Engine"),
-                        aih.CMD_Turn(random.uniform(-36.0, 36.0)),
-                    )
-            # If we're close to a block, we want to stop moving forward
-            # and turn.
-            elif dist <= 2:
-                # Stop moving
-                if speed > 0:
-                    self.cmd_maker.addCmd(
-                        self.tick, self.getSlot("Engine"), aih.CMD_SetSpeed(0.0)
-                    )
-                # Turn in a random direction 18 deg per turn (or 1/20th of a circle).
-                if turn_rate == 0:
-                    self.cmd_maker.addCmd(
-                        self.tick,
-                        self.getSlot("Engine"),
-                        aih.CMD_Turn(random.choice([-18.0, 18.0])),
-                    )
-
-    #Performs appropriate start actions based on starting location
+    # Performs appropriate start actions based on starting location
     def startAction(self, view):
-        
+        print("hey look ma i made it")
+        self.cmd_maker.addCmd(
+            self.tick,
+            self.getSlot("Engine"),
+            self.start_cmd_set[self.cmd_start_iteration],
+        )
+        self.cmd_start_iteration += 1
+        return len(self.start_cmd_set) <= self.cmd_start_iteration
 
     # Implement AI here.
     # IMPORTANT: Must return commands a dictionary that follows the command
@@ -149,10 +153,22 @@ class AI:
 
         self.updateMap(view)
 
-        if not (start_performed):
-            start_performed = self.startAction(view, cmd_iteration)
-        
-        if (start_performed):
+        if (aih.getX(view), aih.getY(view)) == (1, 13):
+            print("aaaa")
+            self.start_cmd_set = self.start_cmds_a
+        elif (aih.getX(view), aih.getY(view)) == (13, 1):
+            print("bbbb")
+            self.start_cmd_set = self.start_cmds_b
+
+        print("ffff")
+        print(self.start_performed)
+        if not (self.start_performed):
+            print("cccc")
+            self.start_performed = self.startAction(view)
+
+        if self.start_performed:
+            print("dddd")
+            print(self.cmd_iteration)
             self.checkForEnemyObj(view, "Red Tank")
             self.determineNextMove(view)
 
