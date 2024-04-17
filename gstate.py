187f46b7d7f06efa299d7171c26df6b5771b1fd7
Sophie Kummer <sophie.kummer@maine.edu>
2024-04-17 08:11:11 -0400

    Refactoring gstate.py to meet code conventions

--- gstate.py
+++ gstate.py
@@ -8,42 +8,52 @@
 
 class GState:
     def __init__(self, data):
+        """Initialize gstate data
+
+        Sets data to input
+        Sets check_state and init_state based on end-state type
+        """
         self.data = data
         self.items = []
         self.objs = []
 
-        self.initState = None
-        self.checkState = None
+        self.init_state = None
+        self.check_state = None
 
         if self.data["type"] == "OBJ_ITEMS_TOUCH":
-            self.checkState = self.checkObjItemsTouch
-            self.initState = self.initObjItemsTouch
+            self.check_state = self.check_obj_items_touch
+            self.init_state = self.init_obj_items_touch
         elif self.data["type"] == "ITEMS_TOUCH":
-            self.checkState = self.checkItemTouch
-            self.initState = self.initItemTouch
+            self.check_state = self.check_item_touch
+            self.init_state = self.init_item_touch
         elif self.data["type"] == "ALL_OBJS_DESTROYED":
-            self.checkState = self.checkAllObjsDestroyed
-            self.initState = self.initAllObjsDestroyed
+            self.check_state = self.check_all_objs_destroyed
+            self.init_state = self.init_all_objs_destroyed
         elif self.data["type"] == "N_OBJS_DESTROYED":
-            self.checkState = self.checkNObjsDestroyed
-            self.initState = self.initNObjsDestroyed
+            self.check_state = self.check_n_objs_destroyed
+            self.init_state = self.init_n_objs_destroyed
 
-    def getData(self, key):
+    def get_data(self, key):
+        """Gets data"""
         return self.data[key]
 
     ############################################################
     # OBJ_ITEM_TOUCH
-    # One of the objects must touch all items simultaneously.
 
-    def initObjItemsTouch(self, objs, items):
+    def init_obj_items_touch(self, objs, items):
+        """Initializes data for object-items-touch end-state
+
+        One of the objects must touch all items simultaneously.
+        """
         for i in items.values():
-            if i.getData("name") in self.data["items"]:
+            if i.get_data("name") in self.data["items"]:
                 self.items.append(i)
         for o in objs.values():
-            if o.getData("name") in self.data["objs"]:
+            if o.get_data("name") in self.data["objs"]:
                 self.objs.append(o)
 
-    def checkObjItemsTouch(self):
+    def check_obj_items_touch(self):
+        """Check if object-item-touch condition has been met"""
         # First see if all items are in the same location
         prev_x = None
         prev_y = None
@@ -51,11 +61,11 @@ class GState:
         state_y = True
         for i in self.items:
             if prev_x is None:
-                prev_x = i.getData("x")
-                prev_y = i.getData("y")
+                prev_x = i.get_data("x")
+                prev_y = i.get_data("y")
             else:
-                state_x = state_x and prev_x == i.getData("x")
-                state_y = state_y and prev_y == i.getData("y")
+                state_x = state_x and prev_x == i.get_data("x")
+                state_y = state_y and prev_y == i.get_data("y")
 
         # If all items are in the same place...
         if state_x and state_y:
@@ -63,7 +73,7 @@ class GState:
             self.data["state"] = False
             # If 1 obj is, set state to true
             for o in self.objs:
-                if prev_x == o.getData("x") and prev_y == o.getData("y"):
+                if prev_x == o.get_data("x") and prev_y == o.get_data("y"):
                     self.data["state"] = True
         else:
             self.data["state"] = False
@@ -72,45 +82,47 @@ class GState:
 
     ############################################################
     # ITEM TOUCH
-    def initItemTouch(self, objs, items):
+    def init_item_touch(self, objs, items):
+        """Initializes data for item-touch end-state"""
         for i in items.values():
-            if i.getData("name") in self.data["items"]:
+            if i.get_data("name") in self.data["items"]:
                 self.items.append(i)
 
-    def checkItemTouch(self):
-
+    def check_item_touch(self):
+        """Checks if item-touch condition has been met"""
         prev_x = None
         prev_y = None
 
-        xstate = True
-        ystate = True
+        state_x = True
+        state_y = True
 
         for i in self.items:
 
             if prev_x is None:
-                prev_x = i.getData("x")
-                prev_y = i.getData("y")
+                prev_x = i.get_data("x")
+                prev_y = i.get_data("y")
             else:
-                xstate = xstate and prev_x == i.getData("x")
-                ystate = ystate and prev_y == i.getData("y")
+                state_x = state_x and prev_x == i.get_data("x")
+                state_y = state_y and prev_y == i.get_data("y")
 
-        self.data["state"] = xstate and ystate
+        self.data["state"] = state_x and state_y
 
         return self.data["state"]
 
     ############################################################
     # ALL OBJS DESTROYED
-    def initAllObjsDestroyed(self, objs, items):
+    def init_all_objs_destroyed(self, objs, items):
+        """Initializes data for all-objects-destroyed end-state"""
         for o in objs.values():
-            if o.getData("name") in self.data["objs"]:
+            if o.get_data("name") in self.data["objs"]:
                 self.objs.append(o)
 
-    def checkAllObjsDestroyed(self):
-
+    def check_all_objs_destroyed(self):
+        """Checks if all-objects-destroyed condition has been met"""
         all_dead = True
 
         for o in self.objs:
-            all_dead = all_dead and not o.getData("alive")
+            all_dead = all_dead and not o.get_data("alive")
 
         self.data["state"] = all_dead
 
@@ -118,17 +130,18 @@ class GState:
 
     ############################################################
     # N OBJS DESTROYED
-    def initNObjsDestroyed(self, objs, items):
+    def init_n_objs_destroyed(self, objs, items):
+        """Initializes data for n-objects-destroyed end-state"""
         for o in objs.values():
-            if o.getData("name") in self.data["objs"]:
+            if o.get_data("name") in self.data["objs"]:
                 self.objs.append(o)
 
-    def checkNObjsDestroyed(self):
-
+    def check_n_objs_destroyed(self):
+        """Checks if n-objects-destroyed condition has been met"""
         num_dead = 0
 
         for o in self.objs:
-            if not o.getData("alive"):
+            if not o.get_data("alive"):
                 num_dead += 1
 
         # Int cast just in case someone adds as string
